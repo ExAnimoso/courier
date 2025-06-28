9479813f44f86bdb188f88960cc05c42b61c8b65
= <exanimoso@yandex.ru>
2025-06-28 19:48:28 +0300

    Added closure cancel on member rejoin

--- bot.py
+++ bot.py
@@ -1486,6 +1486,13 @@ class ModmailBot(commands.Bot):
                 join_message = "The recipient has joined the server."
             embed = discord.Embed(description=join_message, color=self.mod_color)
             await thread.channel.send(embed=embed)
+            await thread.cancel_closure()
+            await thread.channel.send(
+                embed=discord.Embed(
+                    color=self.error_color,
+                    description="Scheduled close has been cancelled.",
+                )
+            )
 
     async def on_message_delete(self, message):
         """Support for deleting linked messages"""
